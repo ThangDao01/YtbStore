fa1fb547ae9ef9997e4d920ff3a8e75a76794283
ThangDao01 <72856471+ThangDao01@users.noreply.github.com>
2021-10-14 23:46:16 +0700

    Add project files.

---
+++ YtbDemo/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace YtbDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        
+        }
+
+        private void getLinkResult_Click(object sender, RoutedEventArgs e)
+        {
+            string html = "<html><head>";
+            html += " <meta content='IE=Edge' http-equiv='X-UA-Compatible'/> ";
+            html += " <iframe id='video' src= 'https://www.youtube.com/embed/{0}' width='600' height='300' frameborder='0' allowfullscreen  </iframe> ";
+            html += " </body></html>";
+            //this.webBrowser1.DocumentText = string.Format(html, txtLink.Text.Split('=')[1]);
+        }
+    }
+}
